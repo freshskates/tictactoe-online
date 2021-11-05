c0d7c8769ddbdc8703d0cea6a567c052dc0f2557
Robert <business.robertcacho@gmail.com>
2021-11-04 17:00:10 -0700

    update

--- client/src/server_config.py
+++ client/src/server_config.py
@@ -1,2 +1,2 @@
-SERVER_IP="http://ec2-3-128-170-247.us-east-2.compute.amazonaws.com:5000"
+SERVER_IP="http://ec2-3-140-249-71.us-east-2.compute.amazonaws.com:5000"
 # SERVER_IP="http://127.0.0.1:5000"
